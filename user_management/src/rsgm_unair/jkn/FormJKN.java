5af46d5328c2f754c5bf6c8efaf919c03655620a
zaidanfy <zaidanfy19@gmail.com>
2021-07-08 19:55:06 +0700

    Menambah FormJKN.java

---
+++ user_management/src/rsgm_unair/jkn/FormJKN.java
@@ -0,0 +1,19 @@
+package rsgm_unair.user_management;
+
+import lombok.AccessLevel;
+import lombok.Setter;
+import lombok.Data;
+import lombok.ToString;
+
+
+@Data public class FormJKN{
+
+	private String nik;
+	private String nama;
+	private String ttl;
+	private String alamat;
+	private String noHP;
+	private String email;
+	private String faskesTingkatI;
+
+}
